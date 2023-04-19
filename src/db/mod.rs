d79c8fa764d43abcf3039f405514a77dfd8a4bdd
Anthony HAMON <hamon.anth@gmail.com>
2023-04-19 23:26:57 +0200

    Add was_displayed field

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -15,7 +15,7 @@ impl Fact {
     }
 
     fn migrate(&self) {
-        let query = "CREATE TABLE facts (fact TEXT UNIQUE, provider TEXT, created_at TEXT);";
+        let query = "CREATE TABLE facts (fact TEXT UNIQUE, provider TEXT, was_displayed TINYINT(1), created_at TEXT);";
         match self.connection.execute(query, ()) {
             Ok(_) => (),
             Err(_) => (),
@@ -25,8 +25,13 @@ impl Fact {
     pub fn create(&self, provider: String, facts: Vec<String>) {
         facts.into_iter().for_each(|f| {
             match self.connection.execute(
-                "INSERT INTO facts VALUES (?1, ?2, ?3);",
-                [(f), (provider.to_owned()), (Local::now().to_string())],
+                "INSERT INTO facts VALUES (?1, ?2, ?3, ?4);",
+                [
+                    (f),
+                    (provider.to_owned()),
+                    (0.to_string()),
+                    (Local::now().to_string()),
+                ],
             ) {
                 Ok(_) => (),
                 Err(_) => (),
@@ -34,10 +39,12 @@ impl Fact {
         });
     }
 
-    pub fn get_random_fact(&self, provider: &str) -> Option<String> {
+    pub fn get_random_fact(&self) -> Option<String> {
         let mut stmt = self
             .connection
-            .prepare("SELECT fact FROM facts WHERE provider = ?1 ORDER BY created_at desc LIMIT 1")
+            .prepare(
+                "SELECT fact FROM facts WHERE was_displayed = 0 ORDER BY created_at DESC LIMIT 1",
+            )
             .unwrap();
         let result = stmt
             .query_map([(provider)], |row: &Row| {
