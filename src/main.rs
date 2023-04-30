e01b9ac7bca07ac2937dd5bd389e5be9f797bc58
Anthony HAMON <hamon.anth@gmail.com>
2023-04-30 14:51:45 +0200

    Add ConfigResolver

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use clokwerk::{Scheduler, TimeUnits};
 use colored::Colorize;
 use daemonize::Daemonize;
-use std::{fs::File, thread, time::Duration};
+use std::{
+    fs::{DirBuilder, File},
+    thread,
+    time::Duration,
+};
 use structopt::StructOpt;
 
 mod db;
 mod reddit;
 mod wikipedia;
 
-const DATABASE_NAME: &str = "/tmp/file.db";
+const DATABASE_NAME: &str = "cultura.db";
 
 #[derive(StructOpt, Debug)]
 #[structopt(
@@ -43,7 +47,9 @@ enum Daemon {
 }
 
 fn main() {
-    let fact = db::Fact::new(DATABASE_NAME);
+    let config_resolver = ConfigResolver::new().unwrap();
+    let database_path = config_resolver.resolve_relative_path(DATABASE_NAME);
+    let fact = db::Fact::new(&database_path);
     let a = Cultura::from_args();
     match a.command {
         Command::FactRoot(provider) => match provider {
@@ -71,18 +77,25 @@ fn main() {
                 let stderr = File::create("/dev/null").unwrap();
 
                 let daemonize = Daemonize::new()
-                    .pid_file("/tmp/cultura.pid")
+                    .pid_file(
+                        config_resolver
+                            .resolve_relative_path("cultura.pid")
+                            .as_str(),
+                    )
                     .working_directory("/tmp")
                     .stdout(stdout)
                     .stderr(stderr)
                     .privileged_action(|| "Executed before drop privileges");
 
+                let d = database_path.clone();
                 let mut scheduler = Scheduler::new();
-                scheduler.every(10.seconds()).run(|| update_facts());
+                scheduler
+                    .every(10.seconds())
+                    .run(move || update_facts(d.as_str()));
 
                 match daemonize.start() {
                     Ok(_) => {
-                        update_facts();
+                        update_facts(&database_path);
                         loop {
                             scheduler.run_pending();
                             thread::sleep(Duration::from_secs(60 * 5));
@@ -95,8 +108,8 @@ fn main() {
     }
 }
 
-fn update_facts() {
-    let fact = db::Fact::new(DATABASE_NAME);
+fn update_facts(database_path: &str) {
+    let fact = db::Fact::new(database_path);
     match reddit::get_til_facts() {
         Ok(v) => fact.create("til".to_string(), v),
         Err(e) => println!("{}", e),
@@ -104,5 +117,38 @@ fn update_facts() {
     match wikipedia::get_dyk_facts() {
         Ok(v) => fact.create("dyk".to_string(), v),
         Err(e) => println!("{}", e),
+    }
+}
+
+#[derive(Clone)]
+struct ConfigResolver {
+    home_dir: String,
+}
+
+impl ConfigResolver {
+    fn new() -> Result<ConfigResolver, ()> {
+        match home::home_dir() {
+            Some(path) => {
+                let c = ConfigResolver {
+                    home_dir: path.display().to_string(),
+                };
+                match DirBuilder::new()
+                    .recursive(true)
+                    .create(c.get_root_config_path())
+                {
+                    Ok(_) => Ok(c),
+                    Err(_) => Err(()),
+                }
+            }
+            None => Err(()),
+        }
+    }
+
+    fn get_root_config_path(&self) -> String {
+        format!("{}/.config/cultura", self.home_dir)
+    }
+
+    fn resolve_relative_path(&self, path: &str) -> String {
+        format!("{}/{}", self.get_root_config_path(), path)
     }
 }
