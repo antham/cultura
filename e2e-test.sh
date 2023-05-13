eb9b99463e28089e27c917a4f624957d3ca704b1
Anthony HAMON <hamon.anth@gmail.com>
2023-05-13 23:34:37 +0200

    Add e2e tests

---
+++ e2e-test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Run e2e tests on cultura
+
+echo "Start the daemon"
+
+cargo run daemon start
+
+cultura_process_count=$(pgrep -c -f cultura)
+
+if [ "$cultura_process_count" -eq 1 ]; then
+    echo "The daemon is started"
+else
+    echo "The daemon is not properly started"
+    exit 1
+fi
+
+cargo run daemon start
+
+cultura_process_count=$(pgrep -c -f cultura)
+
+if [ "$cultura_process_count" -eq 1 ]; then
+    echo "The daemon is started only once"
+else
+    echo "The daemon is not started only once"
+    exit 1
+fi
+
+sleep 10
+
+echo "Check if the generation command works"
+cargo run fact generate-random | grep -E "Do you know that|Today I learned"
