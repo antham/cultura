6944dbc6a91affc63e98ef63c57d2272574f2bf8
Anthony HAMON <hamon.anth@gmail.com>
2023-05-16 00:06:45 +0200

    Remove unused function

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -9,12 +9,6 @@ impl Logger {
         Logger { is_enabled }
     }
 
-    pub fn info(&self, log: &str) {
-        if self.is_enabled {
-            info!("{}", log);
-        }
-    }
-
     pub fn error<S: Into<String>>(&self, err: S) {
         if self.is_enabled {
             error!("{}", err.into());
