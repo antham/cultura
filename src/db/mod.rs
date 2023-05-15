58d78316c62fe421345b1c7f3766ac978c5fa41b
Anthony HAMON <hamon.anth@gmail.com>
2023-05-15 23:03:43 +0200

    Simplify db::Fact

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use chrono::Local;
 use rusqlite::Connection;
 
@@ -6,31 +8,25 @@ pub struct Fact {
 }
 
 impl Fact {
-    pub fn new(path: &str) -> Result<Self, String> {
-        match Connection::open(path) {
-            Ok(connection) => {
-                let f = Fact { connection };
-                match f.migrate() {
-                    Ok(_) => Ok(f),
-                    Err(e) => Err(e),
-                }
-            }
-            Err(e) => Err(format!("cannot open {} : {}", path, e)),
-        }
+    pub fn new(path: &str) -> Result<Self, Box<dyn Error>> {
+        Connection::open(path)
+            .map(|connection| Fact { connection })
+            .map(|f| -> Result<Self, Box<dyn Error>> {
+                f.migrate()?;
+                Ok(f)
+            })?
     }
 
-    fn migrate(&self) -> Result<(), String> {
+    fn migrate(&self) -> Result<(), Box<dyn Error>> {
         let query = "CREATE TABLE IF NOT EXISTS facts (id TEXT UNIQUE, fact TEXT UNIQUE, provider TEXT, was_displayed TINYINT(1), created_at TEXT);";
-        match self.connection.execute(query, ()) {
-            Ok(_) => Ok(()),
-            Err(e) => Err(e.to_string()),
-        }
+        self.connection.execute(query, ())?;
+        Ok(())
     }
 
-    pub fn create(&self, provider: String, facts: Vec<String>) -> Vec<Result<(), String>> {
+    pub fn create(&self, provider: String, facts: Vec<String>) -> Vec<Result<(), Box<dyn Error>>> {
         facts
             .into_iter()
-            .map(|f| {
+            .map(|f| -> Result<(), Box<dyn Error>> {
                 match self.connection.execute(
                     "INSERT INTO facts VALUES (?1, ?2, ?3, ?4, ?5);",
                     [
@@ -42,39 +38,30 @@ impl Fact {
                     ],
                 ) {
                     Ok(_) => Ok(()),
-                    Err(e) => Err(e.to_string()),
+                    Err(e) => Err(e)?,
                 }
             })
-            .collect::<Vec<Result<(), String>>>()
+            .collect::<Vec<Result<(), Box<dyn Error>>>>()
     }
 
-    pub fn get_random_fact(&self) -> Result<Option<(String, String)>, String> {
-        let mut stmt = self
-            .connection
-            .prepare(
-                "SELECT id, fact FROM facts WHERE was_displayed = 0 ORDER BY created_at DESC LIMIT 1",
-            )
-            .unwrap();
-
-        let mut rows = stmt.query([]).unwrap();
-
+    pub fn get_random_fact(&self) -> Result<Option<(String, String)>, Box<dyn Error>> {
+        let mut stmt = self.connection.prepare(
+            "SELECT id, fact FROM facts WHERE was_displayed = 0 ORDER BY created_at DESC LIMIT 1",
+        )?;
+        let mut rows = stmt.query([])?;
         let mut results = Vec::new();
-        while let Some(row) = rows.next().unwrap() {
+        while let Some(row) = rows.next()? {
             results.push(Some((
-                row.get::<usize, String>(0).unwrap(),
-                row.get::<usize, String>(1).unwrap(),
+                row.get::<usize, String>(0)?,
+                row.get::<usize, String>(1)?,
             )))
         }
         Ok(results.pop().unwrap_or(None))
     }
 
-    pub fn mark_as_read(&self, id: String) -> Result<(), String> {
-        match self
-            .connection
-            .execute("UPDATE facts SET was_displayed = 1 WHERE id = ?1", [(id)])
-        {
-            Ok(_) => Ok(()),
-            Err(e) => Err(e.to_string()),
-        }
+    pub fn mark_as_read(&self, id: String) -> Result<(), Box<dyn Error>> {
+        self.connection
+            .execute("UPDATE facts SET was_displayed = 1 WHERE id = ?1", [(id)])?;
+        Ok(())
     }
 }
